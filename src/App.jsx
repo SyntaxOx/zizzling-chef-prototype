af093a1cd6c9645d59d23bce3fe0d7913c3f8ca9
SyntaxOx <131952239+SyntaxOx@users.noreply.github.com>
2025-06-12 13:18:10 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       
-      <BrowserRouter>
+      <BrowserRouter basename="/react">
         <Routes>
           <Route path='/login' element={<Login/>}/>
           <Route path='' element={
